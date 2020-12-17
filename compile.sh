668fbf65fe4ee1b06d15e4db9ecb988ec4a669ce
fabien <contested.space@gmail.com>
2020-12-16 21:06:18 -0500

    Add warning flags to compile script

--- compile.sh
+++ compile.sh
@@ -1,1 +1,13 @@
-gcc src/fabtris2.c src/fabtrimino.c src/next.c -I./src -lSDL2 -lSDL2_image -g -o fabtris2
+CFLAGS="$CFLAGS -Wall -Wextra"
+CFLAGS="$CFLAGS -Wundef"
+CFLAGS="$CFLAGS -Wcast-align"
+CFLAGS="$CFLAGS -Wwrite-strings"
+CFLAGS="$CFLAGS -Wunreachable-code"
+CFLAGS="$CFLAGS -Wformat=2"
+CFLAGS="$CFLAGS -Wswitch-enum"
+CFLAGS="$CFLAGS -Winit-self"
+CFLAGS="$CFLAGS -Wno-implicit-fallthrough"
+
+CFLAGS="$CFLAGS $(sdl2-config --cflags)"
+
+gcc src/fabtris2.c src/fabtrimino.c src/next.c $CFLAGS -I./src -lSDL2 -lSDL2_image -g -std=c11 -o fabtris2
